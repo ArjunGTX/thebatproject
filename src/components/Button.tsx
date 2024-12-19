5fb18a9491e9d3538098ffa32ccb2d364db470ae
ArjunGTX <arjun@usefindr.com>
2024-12-19 10:39:11 +0530

    chore: added cta for downloading resume, directly open links instead of opening a popup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ export type ButtonVariant = "primary";
 
 export const variantClasses: Record<ButtonVariant, string> = {
   primary:
-    "border border-gray-500 rounded-md border-solid text-white/80 bg-black/70 backdrop-blur-sm py-3 px-10  focus-visible:[&:not(:disabled)]:text-white hover:[&:not(:disabled)]:border-white/50 hover:[&:not(:disabled)]:text-white disabled:opacity-30",
+    "border border-gray-500 rounded-md border-solid text-white/80 bg-black/70 backdrop-blur-sm py-3 px-5  focus-visible:[&:not(:disabled)]:text-white hover:[&:not(:disabled)]:border-white/50 hover:[&:not(:disabled)]:text-white disabled:opacity-30",
 };
 
 export interface ButtonProps
