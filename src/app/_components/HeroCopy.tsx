ecc8fe63b2a259f38e833444910a41c1d86efeda
ArjunGTX <arjun@usefindr.com>
2024-12-21 00:06:33 +0530

    chore: improve fade in effect and bounce effect

--- src/app/_components/HeroCopy.tsx
+++ src/app/_components/HeroCopy.tsx
@@ -5,42 +5,51 @@ import { CustomLink } from "@/components/CustomLink";
 import { ExternalLinks } from "@/utils/constants";
 import { motion } from "framer-motion";
 
-const initial = { opacity: 0, x: -500 };
-const animate = { opacity: 1, x: 0 };
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 2.5,
+      staggerChildren: 0.3,
+    },
+  },
+};
 
-const h1Variants = {
-  initial,
-  animate,
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
 };
 
 export const HeroCopy = () => {
   return (
     <motion.div
-      variants={h1Variants}
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      transition={{
-        delay: 0.5,
-        duration: 2.5,
-        staggerChildren: 1.5,
-      }}
+      variants={container}
+      initial="hidden"
+      animate="show"
       className="flex flex-col justify-start gap-6 md:gap-8 items-center text-center order-1 self-start lg:self-center lg:-order-1 lg:text-left lg:items-start"
     >
-      <h1 className="text-5xl text-primary xs:text-6xl font-bebas-neue sm:text-7xl lg:text-9xl xl:text-10xl leading-none font-bold tracking-normal lg:tracking-[0.015em]">
+      <motion.h1
+        variants={item}
+        className="text-5xl text-primary xs:text-6xl font-bebas-neue sm:text-7xl lg:text-9xl xl:text-10xl leading-none font-bold tracking-normal lg:tracking-[0.015em]"
+      >
         Developer by day, <br /> Vigilante by night
-      </h1>
-      <p className="text-base text-secondary sm:text-lg xl:text-xl leading-snug">
+      </motion.h1>
+      <motion.p
+        variants={item}
+        className="text-base text-secondary sm:text-lg xl:text-xl leading-snug"
+      >
         <i>
           &ldquo;If I can&apos;t change things here,{" "}
           <br className="md:hidden" /> if I can&apos;t have an effect, <br />I
           don&apos;t care what happens to me.&rdquo;
         </i>
-      </p>
-      <div className="flex justify-center flex-wrap items-center gap-4">
+      </motion.p>
+      <motion.div
+        variants={item}
+        className="flex justify-center flex-wrap items-center gap-4"
+      >
         <CustomLink
           variant="primary"
           className="flex text-sm md:text-base flex-col justify-center items-center group w-64"
@@ -59,7 +68,7 @@ export const HeroCopy = () => {
           VIGILANTE PROFILE
           <small className="text-tertiary normal-case">(Download resume)</small>
         </CustomLink>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
--- src/app/_components/WorkSection.tsx
+++ src/app/_components/WorkSection.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import React from "react";
 import { PageSection } from "./PageSection";
 import { HomePageSections } from "@/utils/constants";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 0, y: 100 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 100 },
+  show: { opacity: 1, y: 0 },
+};
 
 export const WorkSection = () => {
   return (
@@ -8,13 +27,27 @@ export const WorkSection = () => {
       id={HomePageSections.journey}
       className="w-full snap-center min-h-svh overflow-x-clip pt-[18vh] xs:pt-[18vh] md:pt-[20vh] flex flex-col justify-start items-start"
     >
-      <h2 className="text-4xl md:text-5xl lg:text-7xl font-bebas-neue">
-        The Knight&apos;s journey
-      </h2>
-      <p className="text-secondary max-w-[50ch] text-xs md:text-sm lg:text-lg">
-        Every vigilante needs a backstory. Here&apos;s my professional journey,
-        from sidekick to full-fledged hero of the web development world.
-      </p>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        className="flex flex-col justify-start items-start"
+      >
+        <motion.h2
+          variants={item}
+          className="text-4xl md:text-5xl lg:text-7xl font-bebas-neue"
+        >
+          The Knight&apos;s journey
+        </motion.h2>
+        <motion.p
+          variants={item}
+          className="text-secondary max-w-[50ch] text-xs md:text-sm lg:text-lg"
+        >
+          Every vigilante needs a backstory. Here&apos;s my professional
+          journey, from sidekick to full-fledged hero of the web development
+          world.
+        </motion.p>
+      </motion.div>
       <div className="w-full mt-10 p-[1px] rounded-lg h-[30rem] bg-[radial-gradient(circle_at_80%_60%,_#424242,_#2b2b2b,_#363636,_#3c3c3c,_#232323)]">
         <div className="w-full h-full rounded-lg bg-surface"></div>
       </div>
