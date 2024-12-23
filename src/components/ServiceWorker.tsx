fd5b3900b9708606af820cab3eb3824e85625a1f
ArjunGTX <arjunvc.official@gmail.com>
2024-12-23 14:17:47 +0530

    chore: disable service worker

--- src/components/ServiceWorker.tsx
+++ src/components/ServiceWorker.tsx
@@ -4,7 +4,12 @@ import { useEffect } from "react";
 
 export const ServiceWorker = () => {
   useEffect(() => {
-    if ("serviceWorker" in navigator && process.env.NODE_ENV === "production") {
+    const enableServiceWorker = false;
+    if (
+      "serviceWorker" in navigator &&
+      process.env.NODE_ENV === "production" &&
+      enableServiceWorker
+    ) {
       navigator.serviceWorker.register("/sw.js");
     }
   }, []);
