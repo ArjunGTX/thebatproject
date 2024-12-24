9404f4f36aa1a33c3c6ff024f9cb551da1cffe26
ArjunGTX <arjun@usefindr.com>
2024-12-24 19:20:18 +0530

    chore: load 3d model from cloduflare storage and cache it in browser using service-worker

--- src/components/ServiceWorker.tsx
+++ src/components/ServiceWorker.tsx
@@ -4,12 +4,8 @@ import { useEffect } from "react";
 
 export const ServiceWorker = () => {
   useEffect(() => {
-    const enableServiceWorker = false;
-    if (
-      "serviceWorker" in navigator &&
-      process.env.NODE_ENV === "production" &&
-      enableServiceWorker
-    ) {
+    const enableServiceWorker = true;
+    if ("serviceWorker" in navigator && enableServiceWorker) {
       navigator.serviceWorker.register("/sw.js");
     }
   }, []);
